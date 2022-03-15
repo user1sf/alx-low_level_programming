d1f72479204519d6f38630f0dbb48adf2c7450db
root <silvereflanble@gmail.com>
2022-03-15 05:19:34 -0700

    printing alphabetic letters with putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Documentation: doc
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
