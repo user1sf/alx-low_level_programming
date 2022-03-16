c1500a0bb24ca23bc849f948a4af9ca02920c680
root <silvereflanble@gmail.com>
2022-03-15 23:38:42 -0700

    print numbers in bas3e 10 from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Doc: documentation from task
+ *Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int digit;
+
+	for (digit = 0 ; digit < 10 ; digit++)
+		printf("%d", digit)
+	printf("\n");
+	return (0);
+}
