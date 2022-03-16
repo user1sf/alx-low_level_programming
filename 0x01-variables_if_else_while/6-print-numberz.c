6d55a04efc989eacd3f60ed0eedf85ffe8e6f419
root <silvereflanble@gmail.com>
2022-03-16 02:14:27 -0700

    print numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print-numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Doc: alx task
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 10 ; digit++)
+		putchar(digit);
+	putchar('\n');
+	return (0);
+}
