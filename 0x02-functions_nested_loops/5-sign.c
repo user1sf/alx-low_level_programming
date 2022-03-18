c47c24063001e0b00ebb416f1074708bea5cd9d6
root <silvereflanble@gmail.com>
2022-03-18 06:18:15 -0700

    print the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main"
+/**
+ *print_sign - return 0 letter not lowercase, 1 letter lowercase
+ *@n: the int to print
+ *Return: Always 0
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	else
+	{
+		_putchar('_');
+		return (-1);
+	}
+
+}
