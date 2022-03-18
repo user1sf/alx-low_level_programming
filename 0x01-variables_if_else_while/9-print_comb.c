eea6dc1670080e588ea001a5f380a5bf84af94bb
root <silvereflanble@gmail.com>
2022-03-18 01:52:13 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,13 @@ int main(void)
 	int number;
 
 	for (number = 48; number <= 57; number++)
-		putchar(number);
+	{	putchar(number);
 	if (number == 57)
-		break;
+		{break;}
 	putchar(',');
 	putchar(' ');
+	}
 	putchar('\n');
+
 	return (0);
 }
