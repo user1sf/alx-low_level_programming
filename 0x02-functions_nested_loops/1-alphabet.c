4bef57cb4c20f543e9cce33ac27c2a0b1d687821
root <silvereflanble@gmail.com>
2022-03-18 03:37:48 -0700

    print alphabet task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,14 @@
  *Return: Always 0 (Success)
  *
  */
+
+/*print alphabet task*/
 void print_alphabet(void)
+
 {
 	char alphabet;
 
-	for (alphabbet = 'a'; alphabet <= 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		_putchar(alphabet);
 	_putchar('\n');
-	return (0);
 }
