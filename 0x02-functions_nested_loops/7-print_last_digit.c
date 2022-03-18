1bcf48fde63f87e43ea2ee315adfcf7770977bf2
root <silvereflanble@gmail.com>
2022-03-18 06:39:34 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#innclude "main.h"
+/**
+ *print_last_digit - last digit
+ *@n: the int to print
+ *Return: Always 0
+ */
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		last_digit = (-1 * (n % 10));
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+
+	else
+	{
+		last_digit = (n % 10);
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+}
