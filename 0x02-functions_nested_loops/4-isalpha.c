0fa638f80dde719a730ecd164619f4e263056d9e
root <silvereflanble@gmail.com>
2022-03-18 04:02:56 -0700

    task is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_isalpha - return 1 if c is a letter. lowercase or uppercase
+ *@c: the int to print
+ *Return: Always 0
+ */
+int _isalpha(int c)
+
+{
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
