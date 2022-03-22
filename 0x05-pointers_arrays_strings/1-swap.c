f1ad7a5d566079e4a4cad4738b9ec177833de636
root <silvereflanble@gmail.com>
2022-03-22 11:07:46 -0700

    swap task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *swap_int - swap values of two int a and b
+ *@a: A pointer to an int that will be updated
+ *@b: B pointer to an int that will be updated
+ *Return: void that
+ */
+
+void swap_int(int *a, int *b)
+{
+	int f;
+
+	f = *a;
+	*a = *b;
+	B = f;
+}
