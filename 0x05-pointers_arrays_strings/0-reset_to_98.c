10a1cceff2010440fcc52ae69d0785bd3a6a3002
root <silvereflanble@gmail.com>
2022-03-22 11:03:31 -0700

    print to 98 task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *reset_to_98 - updates an integer value to 98
+ *@n: a pointer to an int that will be updated
+ *Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
