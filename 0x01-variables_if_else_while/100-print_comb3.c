ed953cbcba852b274a446ebf32d391c8ec3f2c2f
root <silvereflanble@gmail.com>
2022-03-23 02:32:54 -0700

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int number_left;
+	int number_right;
+
+	for (number_left  = 48; number_right <= 78; number_left++)
+	{
+		for (number_right = number_left + 1 ; number_right <= 78; number_right++)
+		{
+			putchar(number_left);
+			putchar(number_right);
+
+			if ((number_left == 56) && (number_right == 78))
+			{
+				break;
+			}
+
+			putchar(',');
+			putchar (' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
